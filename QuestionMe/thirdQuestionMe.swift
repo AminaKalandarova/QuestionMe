853354ed7fbed08a37690529f940750f15e88f1f
Amina <kalandarovaaminakhon@gmail.com>
2024-07-30 10:56:16 -0400

    finish

---
+++ QuestionMe/thirdQuestionMe.swift
@@ -0,0 +1,18 @@
+//
+//  thirdQuestionMe.swift
+//  QuestionMe
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct thirdQuestionMe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    thirdQuestionMe()
+}
